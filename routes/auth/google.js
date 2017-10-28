d99e80a5812d87b15cd122cb4351b956161555ea
Yu-Chia Wu <gerrywu@utexas.edu>
2017-10-28 01:52:39 -0500

    Revert "Revert "Add google plus login integration""
    This reverts commit cb9e8a435a2e726c191c00e7b197baf993009b37.

---
+++ routes/auth/google.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../../models/user.js');
+
+var google = require('googleapis');
+var plus = google.plus('v1');
+var OAuth2 = google.auth.OAuth2;
+var oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.HOSTNAME + '/auth/google/callback'
+);
+
+router.get('/auth/google', function(request, response) {
+  var url = oauth2Client.generateAuthUrl({
+    access_type: 'offline', // will return a refresh token
+    scope: [
+      'https://www.googleapis.com/auth/plus.me',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ] // can be a space-delimited string or an array of scopes
+  });
+
+  response.redirect(url);
+})
+
+router.get('/auth/google/callback', function(request, response) {
+  let code = request.query.code;
+  oauth2Client.getToken(code, function(err, tokens) {
+    if (err) {
+      response.send(err);
+    } else {
+      oauth2Client.setCredentials(tokens);
+      plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, profile) {
+        if (err) {
+          console.log(err);
+        } else {
+          // Create a user account based on the email and redirect them back to Yelp page.
+          User.findOne({email: profile.emails[0].value}).exec(function(err, user) {
+            console.log(user);
+            if (user === null) {
+              let name = profile.displayName;
+              let email = profile.emails[0].value;
+
+              var new_user = new User({
+                name: name,
+                email: email
+              });
+
+              new_user.save(function(err) {
+                if(err) {
+                  console.log(err);
+                } else {
+                  request.session.email = email;
+                  request.session.name = name;
+                }
+              });
+            } else {
+              request.session.email = user.email;
+              request.session.name = user.name;
+              response.redirect('/yelp');
+            }
+          });
+        }
+      });
+    }
+  })
+})
+
+module.exports = router;
